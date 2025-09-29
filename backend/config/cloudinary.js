d5b932a35509641afbe2ab7fdd66a3b8b31fe7f8
Ashutosh Sahu <ashutoshsahu9601@gmail.com>
2025-09-29 17:27:18 +0530

    correct cloudinary

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -9,7 +9,7 @@ const uploadOnCloudinary=async (filepath)=>{
     cloudinary.config({ 
         cloud_name: process.env.CLOUDINARY_NAME, 
         api_key: process.env.CLOUDINARY_API_KEY,
-        api_secret: process.env.CLOUDIRY_API_SECRET
+        api_secret: process.env.CLOUDINARY_API_SECRET
     });
     try {
         if(!filepath)
